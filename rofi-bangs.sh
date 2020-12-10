d1dcd2c43f338d659356197f855561d75f08918c
filipeoliv02 <filipeh291@gmail.com>
2020-12-10 22:57:38 +0000

    first commit

---
+++ rofi-bangs.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+# info: this is a script to launch other rofi scripts,
+#       saves us the trouble of binding multiple hotkeys for each script,
+#       when we can just use one hotkey for everything.
+
+declare -A LABELS
+declare -A COMMANDS
+
+###
+# List of defined 'bangs'
+
+# Emojis
+COMMANDS["Emojis"]="rofimoji"
+LABELS["Emojis"]=""
+
+# Twitch
+COMMANDS["Twitch"]="kpl"
+LABELS["Twitch"]=""
+
+# Calculator
+COMMANDS["Calculator"]="rofi -show calc"
+LABELS["Calculator"]=""
+
+
+################################################################################
+# do not edit below
+################################################################################
+##
+# Generate menu
+##
+function print_menu()
+{
+    for key in ${!LABELS[@]}
+    do
+  echo "$key    ${LABELS}"
+     #   echo "$key    ${LABELS[$key]}"
+     # my top version just shows the first field in labels row, not two words side by side
+    done
+}
+##
+# Show rofi.
+##
+function start()
+{
+    # print_menu | rofi -dmenu -p "?=>" 
+    print_menu | sort | rofi -dmenu -i -p "Scripts"
+
+}
+
+
+# Run it
+value="$(start)"
+
+# Split input.
+# grab upto first space.
+choice=${value%%\ *}
+# graph remainder, minus space.
+input=${value:$((${#choice}+1))}
+
+##
+# Cancelled? bail out
+##
+if test -z ${choice}
+then
+    exit
+fi
+
+# check if choice exists
+if test ${COMMANDS[$choice]+isset}
+then
+    # Execute the choice
+    eval echo "Executing: ${COMMANDS[$choice]}"
+    eval ${COMMANDS[$choice]}
+else
+ eval  $choice | rofi
+ # prefer my above so I can use this same script to also launch apps like geany or leafpad etc (DK) 
+ #   echo "Unknown command: ${choice}" | rofi -dmenu -p "error"
+fi
